1692989eeca1a0118a76b18b909f70f4e5067f8e
JordanH <jordanh@apadmi.com>
2024-02-15 20:31:05 +0000

    Remvoe xbase

--- lua/plugins/xbase.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "wojciech-kulik/xcodebuild.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim" },
-  config = function()
-    require("xcodebuild").setup({
-      -- put some options here or leave it empty to use default settings
-    })
-  end,
-}
