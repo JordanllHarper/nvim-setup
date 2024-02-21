f09e9505c30cd98bf77fa47314fff817c8875d81
JordanH <jordanh@apadmi.com>
2024-02-21 15:19:58 +0000

    Add project config

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -5,17 +5,56 @@ return {
     'nvim-lua/plenary.nvim',
     'stevearc/dressing.nvim', -- optional for vim.ui.select
   },
-  opts = {
-    widget_guides = {
-      enabled = true,
-    },
-    dev_tools = {
-      autostart = true
-    },
-    lsp = {
-      color = {
-        enabled = true,
+  config = function()
+    require('flutter-tools').setup {
+      {
+        widget_guides = {
+          enabled = true,
+        },
+        dev_tools = {
+          autostart = true
+        },
+        lsp = {
+          color = {
+            enabled = true,
+          }
+        },
+      },
+
+    }
+    require('flutter-tools').setup_project({
+      {
+        name = 'Mobile',                  -- an arbitrary name that you provide so you can recognise this config
+        target = 'lib/main_preProd.dart', -- your target
+        device = 'emulator-5554',         -- the device ID, which you can get by running `flutter devices`
+        dart_define = {
+          API_URL = 'https://dev.example.com/api',
+          IS_DEV = true,
+        },
+        dart_define_from_file = 'config.json' -- the path to a JSON configuration file
+      },
+
+      {
+        name = 'Tablet',                  -- an arbitrary name that you provide so you can recognise this config
+        target = 'lib/main_preProd.dart', -- your target
+        device = 'emulator-5556',         -- the device ID, which you can get by running `flutter devices`
+        dart_define = {
+          API_URL = 'https://dev.example.com/api',
+          IS_DEV = true,
+        },
+        dart_define_from_file = 'config.json' -- the path to a JSON configuration file
+      },
+      {
+        name = 'Web',
+        device = 'chrome',
+        flavor = 'preProd',
+        target = 'lib/main_preProd.dart', -- your target
+        web_port = 5000
+      },
+      {
+        name = 'Profile',
+        flutter_mode = 'profile', -- possible values: `debug`, `profile` or `release`, defaults to `debug`
       }
-    },
-  },
+    })
+  end
 }
