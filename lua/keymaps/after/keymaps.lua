ab57ae7d0064b138c33d62a0055afb56bb263542
JordanH <jordanh@apadmi.com>
2024-02-21 13:26:25 +0000

    Add display file keybinding

--- lua/keymaps/after/keymaps.lua
+++ lua/keymaps/after/keymaps.lua
@@ -1,6 +1,7 @@
 local nvim_tree_api = require "nvim-tree.api".tree
 
-vim.keymap.set('n', '<leader>df', nvim_tree_api.toggle, { desc = 'nvim-tree: [d]isplay [f]iles (toggle)' })
+vim.keymap.set('n', '<leader>df', nvim_tree_api.toggle, { desc = 'nvim-tree: [D]isplay [F]iles (toggle)' })
+vim.keymap.set('n', '<leader>dt', nvim_tree_api.find_file, { desc = 'nvim-tree: [D]isplay in [T]ree (toggle)' })
 
 -- [[ Basic Keymaps ]]
 
