457c4829454a7ec85c554e208262c8b9f46e78a1
JordanH <jordanh@apadmi.com>
2024-02-21 22:47:23 +0000

    Fidget stuff and solving mason lsp
    Removed the custom handlers (never use them)

--- lua/plugins/lsp_config_with_plugins.lua
+++ lua/plugins/lsp_config_with_plugins.lua
@@ -6,30 +6,22 @@ return {
     { 'williamboman/mason.nvim', },
     { 'williamboman/mason-lspconfig.nvim', },
     -- Useful status updates for LSP
-    { 'j-hui/fidget.nvim',                 tag = 'legacy', opts = {} },
-
+    { 'j-hui/fidget.nvim',                 opts = {} },
     {
       'folke/neodev.nvim',
       opts = {}
     },
   },
   config = function()
-    local servers = require('lsp.mason_servers')
     local on_attach_keymaps = require('keymaps.lsp-attach')
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
     require('mason').setup()
-    local mason_lspconfig = require 'mason-lspconfig'
-    mason_lspconfig.setup {
-      ensure_installed = vim.tbl_keys(servers),
-    }
-    mason_lspconfig.setup_handlers {
+    require('mason-lspconfig').setup_handlers {
       function(server_name)
         require('lspconfig')[server_name].setup {
           capabilities = capabilities,
           on_attach = on_attach_keymaps,
-          settings = servers[server_name],
-          filetypes = (servers[server_name] or {}).filetypes,
         }
       end,
     }
