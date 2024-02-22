753110a863d1564d9551b293b278cfa381896ee2
JordanH <jordanh@apadmi.com>
2024-02-22 13:59:49 +0000

    Ignore install for dart

--- lua/additional-config/treesitter-setup.lua
+++ lua/additional-config/treesitter-setup.lua
@@ -1,15 +1,16 @@
 vim.defer_fn(function()
 	require('nvim-treesitter.configs').setup {
-		sync_install          = true,
-		ignore_install        = {},
-		modules               = {},
+		sync_install     = true,
+		ignore_install   = { 'dart' },
+		modules          = {},
 		-- Add languages to be installed here that you want installed for treesitter
-		ensure_installed      = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript',
+		ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript',
 			'vimdoc', 'vim',
 			'bash' },
 
 		-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-		auto_install          = true,
+		auto_install     = true,
+
 
 		highlight             = { enable = true },
 		indent                = { enable = true },
