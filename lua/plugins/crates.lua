d236b5427c356d854f06aeb07b4bc11d0f0f89de
JordanH <jordanh@apadmi.com>
2024-02-22 16:30:37 +0000

    Remove src

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -2,12 +2,5 @@ return {
   'saecki/crates.nvim',
   tag = 'v0.4.0',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts =  {
-      src = {
-      --   cmp = {
-      --     enabled = true
-      -- }
-    }
-  }
+  config = true
 }
-
