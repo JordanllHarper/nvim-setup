b8230c0529445f1bfa09214ffdad4a4b75ce57b5
JordanH <jordanh@apadmi.com>
2024-02-22 16:31:52 +0000

    Update more plugins

--- lua/plugins/dev-utils.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'globals',
-  dir = '/Users/jordanh/plugin-dev/globals.nvim/',
-  config = function()
-    require("globals")
-  end
-}
---
+++ lua/plugins/lua-line.lua
@@ -0,0 +1,25 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  opts = {
+    sections = {
+      lualine_x = {
+        {
+          require("lazy.status").updates,
+          cond = require("lazy.status").has_updates,
+          color = { fg = "#ff9e64" },
+        },
+      },
+
+    },
+    options = {
+      icons_enabled = true,
+      theme = 'tokyonight',
+
+      component_separators = { left = '', right = '' },
+
+      section_separators = { left = '', right = '' },
+    },
+  },
+
+  dependencies = { 'nvim-tree/nvim-web-devicons', opts = true }
+}
