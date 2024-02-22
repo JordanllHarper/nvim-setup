c25fb320ef8b41c34f5a12972b27ab5e83b18670
JordanH <jordanh@apadmi.com>
2024-02-22 16:38:07 +0000

    Theme update

--- lua/themes/tokynight.lua
+++ lua/themes/tokynight.lua
@@ -1,16 +1,12 @@
 return {
-
 	-- Theme inspired by Atom
-	{
-		'folke/tokyonight.nvim',
-		lazy = false,
-		priority = 1000,
-		opts = {
-			style = 'moon'
-		},
-		config = function()
-			vim.cmd([[colorscheme tokyonight]])
-		end
-	}
+
+	'folke/tokyonight.nvim',
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd([[colorscheme tokyonight]])
+	end
+
 
 }
