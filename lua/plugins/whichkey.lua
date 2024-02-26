4af2f23471a5fe017993d6679d8aa1b5f948391c
JordanH <jordanh@apadmi.com>
2024-02-26 15:23:41 +0000

    Add more which key

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -7,10 +7,12 @@ return {
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
       ['<leader>h'] = { name = '[H]over', _ = 'which_key_ignore' },
       ['<leader>nf'] = { name = '[F]...', _ = 'which_key_ignore' },
+      ['<leader>f'] = { name = '[F]lutter', _ = 'which_key_ignore' },
       ['<leader>l'] = { name = '[L]azy', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>n'] = { name = '[N]vim Tree', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+      ['<leader>fr'] = { name = '[R]e...', _ = 'which_key_ignore' },
     }
   end
 }
