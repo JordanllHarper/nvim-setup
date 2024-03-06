e188a531a56b4fd01148e49002d7eaf1b1bd304f
JordanH <jordanh@apadmi.com>
2024-03-06 09:48:47 +0000

    Remove delete all

--- lua/keymaps/keymaps.lua
+++ lua/keymaps/keymaps.lua
@@ -1,3 +1,2 @@
 -- buffer commands
 vim.keymap.set('n', '<leader>bd', '<Cmd>bd<CR>', { silent = true, desc = '[B]uffer [D]elete (current)' })
-vim.keymap.set('n', '<leader>bD', '<Cmd>.,$-bdelete<CR>', { silent = true, desc = '[B]uffer [D]elete (all)' })
