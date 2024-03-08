19299303baeeb24dbb97be4f9349b52f7f0ef033
JordanH <jordanh@apadmi.com>
2024-03-08 14:12:14 +0000

    Quick fix keymaps

--- lua/keymaps/keymaps.lua
+++ lua/keymaps/keymaps.lua
@@ -1,2 +1,6 @@
 -- buffer commands
 vim.keymap.set('n', '<leader>bd', '<Cmd>bd<CR>', { silent = true, desc = '[B]uffer [D]elete (current)' })
+
+-- Quickfix
+vim.keymap.set('n', '<leader>qa', ':vim', { silent = true, desc = '[Q]uickfix [A]dd' })
+vim.keymap.set('n', '<leader>qo', '<Cmd>copen<CR>', { silent = true, desc = '[Q]uickfix [O]pen' })
