ae6f866cbebed7391c5bc1d1645708c2f177c385
JordanH <jordanh@apadmi.com>
2024-03-21 11:52:16 +0000

    Add terminal keymap

--- lua/keymaps/keymaps.lua
+++ lua/keymaps/keymaps.lua
@@ -4,3 +4,6 @@ vim.keymap.set('n', '<leader>bd', '<Cmd>bd<CR>', { silent = true, desc = '[B]uff
 -- Quickfix
 vim.keymap.set('n', '<leader>qa', ':vim', { silent = true, desc = '[Q]uickfix [A]dd' })
 vim.keymap.set('n', '<leader>qo', '<Cmd>copen<CR>', { silent = true, desc = '[Q]uickfix [O]pen' })
+
+-- Terminal
+vim.keymap.set('n', '<leader>t', '<Cmd>term<CR>', { silent = true, desc = '[T]erminal' })
