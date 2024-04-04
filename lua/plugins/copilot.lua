248a2a399c0695d1ed7ae12d9e8f66d9b24ebf47
JordanH <jordanh@apadmi.com>
2024-04-04 14:39:50 +0100

    Add maps for copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,9 +2,17 @@ return {
   'github/copilot.vim',
   config = function()
     -- Copilot bindings
-    vim.keymap.set('i', '<A-j>', 'copilot#Accept(\"<CR>\")', {
+    vim.keymap.set('i', '<D-j>', 'copilot#Accept(\"<CR>\")', {
       expr = true,
       replace_keycodes = false
+    })
+
+    vim.keymap.set('n', '<leader>ce', '<Cmd>Copilot enable<CR>', {
+      desc = "[C]opilot [E]nable"
+    })
+
+    vim.keymap.set('n', '<leader>cd', '<Cmd>Copilot disable<CR>', {
+      desc = "[C]opilot [D]isable"
     })
   end
 }
--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -5,8 +5,13 @@ return {
     timeout = 7000
   },
 
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+  },
+
   config = function(_, opts)
     require('notify').setup(opts)
+    require('telescope').extensions.notify.notify(opts)
     vim.notify = require("notify")
   end
 }
