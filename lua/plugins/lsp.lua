7dc68742b036227e6d94f4e562e24a417b43fe25
JordanH <jordanh@apadmi.com>
2024-04-15 14:05:54 +0100

    Change virtual text to only show errors

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,6 +13,12 @@ return {
   config = function()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+    vim.diagnostic.config({
+      virtual_text = {
+        severity = vim.diagnostic.severity.E
+      }
+    })
+
     require('mason').setup()
     require('mason-lspconfig').setup_handlers {
       function(server_name)
