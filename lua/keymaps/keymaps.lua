9345eb963c391396a6ec750394411c5301eacd7d
JordanH <jordanh@apadmi.com>
2024-04-19 20:27:51 +0100

    updating keymaps to remove sizing until we can fix <CR> new space bug

--- lua/keymaps/keymaps.lua
+++ lua/keymaps/keymaps.lua
@@ -1,8 +1,4 @@
--- buffer commands
-vim.keymap.set('n', '<leader>bd', '<Cmd>bd<CR>', { silent = true, desc = '[B]uffer [D]elete (current)' })
-
 -- Quickfix
-vim.keymap.set('n', '<leader>qa', ':vim', { silent = true, desc = '[Q]uickfix [A]dd' })
 vim.keymap.set('n', '<leader>qo', '<Cmd>copen<CR>', { silent = true, desc = '[Q]uickfix [O]pen' })
 
 -- Terminal
@@ -13,8 +9,8 @@ vim.keymap.set('t', '<C-q>', '<C-\\><C-n>', { silent = true, desc = '[Q]uit term
 vim.keymap.set('n', '<leader>Li', '<Cmd>LspInfo<CR>', { silent = true, desc = '[L]sp [I]nfo' })
 vim.keymap.set('n', '<leader>Lr', '<Cmd>LspRestart<CR>', { silent = true, desc = '[L]sp [R]estart' })
 
--- Window resizing
-vim.keymap.set('n', '<C-m>', '<Cmd>vertical resize +2<CR>', { silent = true, desc = 'Increase window height' })
-vim.keymap.set('n', '<C-n>', '<Cmd>vertical resize -2 <CR>', { silent = true, desc = 'Decrease window height' })
-vim.keymap.set('n', '<C-b>', '<Cmd>horizontal resize +2<CR>', { silent = true, desc = 'Increase window width' })
-vim.keymap.set('n', '<C-s>', '<Cmd>horizontal resize -2<CR>', { silent = true, desc = 'Decrease window width' })
+-- -- Window resizing
+-- vim.keymap.set('n', '<C-m>', '<Cmd>vertical resize +2<CR>', { silent = true, desc = 'Increase window height' })
+-- vim.keymap.set('n', '<C-n>', '<Cmd>vertical resize -2 <CR>', { silent = true, desc = 'Decrease window height' })
+-- vim.keymap.set('n', '<C-b>', '<Cmd>horizontal resize +2<CR>', { silent = true, desc = 'Increase window width' })
+-- vim.keymap.set('n', '<C-s>', '<Cmd>horizontal resize -2<CR>', { silent = true, desc = 'Decrease window width' })
