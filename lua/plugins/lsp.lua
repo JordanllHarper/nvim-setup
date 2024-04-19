fe4f8ff64b6fd1746cd079d7f541646414c7c828
JordanH <jordanh@apadmi.com>
2024-04-19 19:04:46 +0100

    remove mason
    We are going forward with manual impls for servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,33 +1,66 @@
 return {
-
   -- LSP Configuration & Plugins
   'neovim/nvim-lspconfig',
   dependencies = {
-    -- Automatically install LSPs to stdpath for neovim
-    { 'williamboman/mason.nvim', },
-    { 'williamboman/mason-lspconfig.nvim', },
     -- Useful status updates for LSP
-    { 'j-hui/fidget.nvim',                 opts = {} },
-    { 'folke/neodev.nvim',                 opts = {} },
+    { 'j-hui/fidget.nvim', opts = {} },
+    { 'folke/neodev.nvim', opts = {} },
   },
   config = function()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
     vim.diagnostic.config({
       virtual_text = {
         severity = vim.diagnostic.severity.E
       }
     })
+    -- Server setup
 
-    require('mason').setup()
-    require('mason-lspconfig').setup_handlers {
-      function(server_name)
-        require('lspconfig')[server_name].setup {
-          capabilities = capabilities,
-          on_attach = require('keymaps.lsp-attach'),
-        }
+    require 'lspconfig'.lua_ls.setup {
+      on_attach = function(client, bufnr)
+        require('lua.keymaps.lsp-attach')(client, bufnr)
       end,
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+          return
+        end
 
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+              -- Depending on the usage, you might want to add additional paths here.
+              -- "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            }
+            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+            -- library = vim.api.nvim_get_runtime_file("", true)
+          }
+        })
+      end,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' }
+          }
+        }
+      }
+    }
+    require 'lspconfig'.rust_analyzer.setup {
+      on_attach = function(client, bufnr)
+        vim.lsp.inlay_hint.enable()
+        require('keymaps.lsp-attach')(client, bufnr)
+      end,
+      capabilities = capabilities,
     }
   end
 }
