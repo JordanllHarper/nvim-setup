471a63fbad2d232e8810b764db6e0f348b8b7cd6
JordanH <jordanh@apadmi.com>
2024-04-19 20:28:16 +0100

    remove movement keys from bindings

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -2,6 +2,15 @@ return {
   'stevearc/overseer.nvim',
   opts = {
     templates = { 'builtin', "user.dart_build", "user.dart_test" },
+    task_list = {
+      bindings = {
+        -- disable so we can use C-j/k to navigate
+        ["<C-k>"] = false,
+        ["<C-j>"] = false,
+        ["<C-l>"] = false,
+        ["<C-h>"] = false,
+      }
+    }
   },
   config = function(_, opts)
     require('overseer').setup(opts)
