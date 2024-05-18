d7d39bdc20621213fd9ea7632f28ae5dc28cbd2a
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2024-05-18 10:41:06 +0100

    add snippets

---
+++ lua/custom/snippets/dart.lua
@@ -0,0 +1,20 @@
+require('luasnip.session.snippet_collection').clear_snippets "dart"
+local ls = require "luasnip"
+
+local s = ls.snippet
+local i = ls.insert_node
+
+
+local fmt = require "luasnip.extras.fmt".fmt
+local fmta = require "luasnip.extras.fmt".fmta
+
+ls.add_snippets("dart", {
+	s("scaffold", fmt("return Scaffold(body: {});", { i(1) })),
+	s("cubit", fmta([[
+class (name)Cubit extends Cubit<(state)>{
+
+	(finish)
+
+}
+]], { name = i(1), state = i(2), finish = i(0) }, { delimiters = "()" }))
+})
---
+++ lua/custom/snippets/lua.lua
@@ -0,0 +1,19 @@
+require('luasnip.session.snippet_collection').clear_snippets "dart"
+local ls = require "luasnip"
+
+local s = ls.snippet
+local i = ls.insert_node
+
+
+local fmt = require "luasnip.extras.fmt".fmt
+local fmta = require "luasnip.extras.fmt".fmta
+
+
+ls.add_snippets("lua", {
+	s("keymap", fmt("vim.keymap.set(\"{}\", \"{}\", {}, {})", {
+		i(1),
+		i(2),
+		i(3),
+		i(4),
+	}))
+})
