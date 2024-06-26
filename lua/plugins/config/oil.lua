2d87c0c04d9de6fa777bd94cc14312ea88e11a0d
JordanH <jordanh@apadmi.com>
2024-06-26 09:50:19 +0100

    Update to actually disable keymaps

--- lua/plugins/config/oil.lua
+++ lua/plugins/config/oil.lua
@@ -1,10 +1,10 @@
 local nmap = require('utils').nmap
 local opts = {
   -- Disable so we can move
-  ["<C-h>"] = false,
-  ["<C-l>"] = false,
-  ["<C-j>"] = false,
-  ["<C-k>"] = false,
+  keymaps = {
+    ["<C-h>"] = false,
+    ["<C-l>"] = false,
+  }
 }
 require('oil').setup(opts)
 nmap("-", "<Cmd>Oil<CR>", "[-] Oil")
